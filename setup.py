d9d28a9002b6b3a24d409ef817f156e3beccf06e
Keyvan Salehi <KSalehi@LESMILLS.CO.NZ>
2025-11-27 09:23:04 +1300

    create the setup function info

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages
+
+setup(
+name='mlops project',
+description='This is the mlops projects for retension memberships for lesmills',
+author='k1',
+author_email='keyvan.salehi@lesmills.co.nz',
+packages=find_packages()
+)
